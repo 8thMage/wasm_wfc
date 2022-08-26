76be9b9ed67e1cc4df6570c8cb3a623afab78c46
the 8th mage <eee34353@gmail.com>
2022-08-26 19:07:47 +0300

    another change to index

--- www/index.js
+++ www/index.js
@@ -1,4 +1,3 @@
-import init from "../pkg/wasm_wfc.js";
+import init, {start} from "../pkg/wasm_wfc.js";
 await init("../pkg/wasm_game_of_life_bg.wasm");
-import * as wasm_wfc from "../pkg/wasm_wfc.js";
-wasm_wfc.start()
+start()
