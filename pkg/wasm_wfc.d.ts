a6ed10b2e9101fedb42e1015a466755b916829f8
the 8th mage <eee34353@gmail.com>
2022-08-26 13:40:39 +0300

    Revert "pkg"
    This reverts commit c4565830df60c61ef40cd792be87430fe3730870.

--- pkg/wasm_wfc.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-*/
-export function start(): void;
-/**
-*/
-export function greet(): void;
--- pkg/wasm_wfc.js
+++
@@ -1,2 +0,0 @@
-import * as wasm from "./wasm_wfc_bg.wasm";
-export * from "./wasm_wfc_bg.js";
