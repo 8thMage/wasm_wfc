500dc8cb68e0c0fd37afa60f71e4185bc7ca31f8
the 8th mage <eee34353@gmail.com>
2022-08-26 18:59:07 +0300

    index

--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-import * as wasm_wfc from "../pkg/wasm_wfc.js";
 import init from "../pkg/wasm_wfc.js";
 await init("../pkg/wasm_game_of_life_bg.wasm");
+import * as wasm_wfc from "../pkg/wasm_wfc.js";
 wasm_wfc.start()
