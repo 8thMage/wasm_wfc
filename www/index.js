5b18755ee0eb63422623c135248ac65ec6004787
the 8th mage <eee34353@gmail.com>
2022-08-27 18:40:49 +0300

    index fix

--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-// import init, {start} from "../pkg/wasm_wfc.js";
-// await init("../pkg/wasm_wfc_bg.wasm");
-import {start} from "../pkg/wasm_wfc.js";
+import init, {start} from "../pkg/wasm_wfc.js";
+await init("../pkg/wasm_wfc_bg.wasm");
+// import {start} from "../pkg/wasm_wfc.js";
 start()
